3b95e2b6305d75f6bf095e9af4e23de7f3d09693
Carmelo Buelvas <cbuelvasc@gmail.com>
2024-03-28 13:08:49 -0500

    update README file

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -60,9 +60,7 @@ export const CounterApp = ({ value }) => {
               </h1>
               <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">{counter}</h2>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
+              Application built with React, Vite and Tailwind CSS.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <button
